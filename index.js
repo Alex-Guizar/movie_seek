27a5a7c20bad47089849609a4762ec2cc47119b7
Alex Guizar <alexguizar90@gmail.com>
2021-07-29 21:58:09 -0700

    Updated movie list with more information

--- index.js
+++ index.js
@@ -5,43 +5,73 @@ const app = express();
 let topMovies = [
   {
     title: 'The Departed',
-    director: 'Martin Scorsese'
+    director: 'Martin Scorsese',
+    genre: 'crime',
+    description: 'An undercover cop and a mole in the police attempt to identify each other while infiltrating an Irish gang in South Boston.',
+    imageURL: ''
   },
   {
     title: 'Inglourious Basterds',
-    director: 'Quentin Tarantino'
+    director: 'Quentin Tarantino',
+    genre: 'adventure',
+    description: 'In Nazi-occupied France during World War II, a plan to assassinate Nazi leaders by a group of Jewish U.S. soldiers coincides with a theatre owner\'s vengeful plans for the same.',
+    imageURL: ''
   },
   {
     title: 'The Shawshank Redemption',
-    director: 'Frank Darabont'
+    director: 'Frank Darabont',
+    genre: 'drama',
+    description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+    imageURL: ''
   },
   {
     title: 'The Dark Knight',
-    director: 'Christopher Nolan'
+    director: 'Christopher Nolan',
+    genre: 'action',
+    description: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+    imageURL: ''
   },
   {
     title: 'Pulp Fiction',
-    director: 'Quentin Tarantino'
+    director: 'Quentin Tarantino',
+    genre: 'crime',
+    description: 'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.',
+    imageURL: ''
   },
   {
     title: 'Fight Club',
-    director: 'David Fincher'
+    director: 'David Fincher',
+    genre: 'drama',
+    description: 'An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.',
+    imageURL: ''
   },
   {
     title: 'Spirited Away',
-    director: 'Hayao Miyazaki'
+    director: 'Hayao Miyazaki',
+    genre: 'animation',
+    description: 'During her family\'s move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.',
+    imageURL: ''
   },
   {
     title: 'Gladiator',
-    director: 'Ridley Scott'
+    director: 'Ridley Scott',
+    genre: 'action',
+    description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.',
+    imageURL: ''
   },
   {
     title: 'Superbad',
-    director: 'Greg Mottola'
+    director: 'Greg Mottola',
+    genre: 'comedy',
+    description: 'Two co-dependent high school seniors are forced to deal with separation anxiety after their plan to stage a booze-soaked party goes awry.',
+    imageURL: ''
   },
   {
     title: 'Inception',
-    director: 'Christopher Nolan'
+    director: 'Christopher Nolan',
+    genre: 'action',
+    description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.',
+    imageURL: ''
   }
 ];
 
