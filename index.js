47066ff2e3c2fe1b4d03707ed6d56e600a7b0c8a
Alex Guizar <alexguizar90@gmail.com>
2021-09-03 14:56:17 -0700

    Added validation to user creation and updating

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express'),
   bodyParser = require('body-parser'),
   morgan = require('morgan'),
   mongoose = require('mongoose'),
+  { check, validationResult } = require('express-validator'),
   Models = require('./models.js');
 const app = express();
 const Movies = Models.Movie;
@@ -12,6 +13,10 @@ mongoose.connect('mongodb://localhost:27017/movieSeek', { useNewUrlParser: true,
 app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Add cors to app
+const cors = require('cors');
+app.use(cors());
 
 // Import auth and passport
 let auth = require('./auth')(app);
@@ -84,54 +89,84 @@ app.get('/users/:username', (req, res) => {
 });
 
 // Add data for new user to our list of users
-app.post('/users', (req, res) => {
-  Users.findOne({ Username: req.body.Username })
-    .then((user) => {
-      if (user) {
-        return res.status(400).send(req.body.Username + ' already exists');
-      } else {
-        Users.create({
-          Username: req.body.Username,
-          Password: req.body.Password,
-          Email: req.body.Email,
-          Birthday: req.body.Birthday
-        })
-        .then((user) => res.status(201).json(user))
-        .catch((err) => {
-          console.error(err);
-          res.status(500).send('Error: ' + err);
-        });
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
-});
+app.post('/users',
+  // Validation logic here for requests
+  [
+    check('Username', 'Username is required',).isLength({ min: 5 }),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], (req, res) => {
+    //Chech the validation object for errors
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
 
-// Update data for specified user
-app.put('/users/:username', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Users.findOneAndUpdate({ 'Username': req.params.username }, {
-      $set: {
-        Username: req.body.Username,
-        Password: req.body.Password,
-        Email: req.body.Email,
-        Birthday: req.body.Birthday
-      }
-    },
-    { new: true },
-    (err, updatedUser) => {
-      if (err) {
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    Users.findOne({ Username: req.body.Username })
+      .then((user) => {
+        if (user) {
+          return res.status(400).send(req.body.Username + ' already exists');
+        } else {
+          Users.create({
+            Username: req.body.Username,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) => res.status(201).json(user))
+          .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+          });
+        }
+      })
+      .catch((err) => {
         console.error(err);
         res.status(500).send('Error: ' + err);
-      } else {
-        res.status(200).json(updatedUser);
-      }
-    });
-});
+      });
+  });
+
+// Update data for specified user
+app.put('/users/:username', passport.authenticate('jwt', { session: false }),
+  [
+    check('Username', 'Username is required',).isLength({ min: 5 }),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ],
+  (req, res) => {
+    //Chech the validation object for errors
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    let hashedPassword = Users.hashPassword(req.body.Password);
+
+    Users.findOneAndUpdate({ 'Username': req.params.username }, {
+        $set: {
+          Username: req.body.Username,
+          Password: hashedPassword,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday
+        }
+      },
+      { new: true },
+      (err, updatedUser) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send('Error: ' + err);
+        } else {
+          res.status(200).json(updatedUser);
+        }
+      });
+  });
 
 // Add movie to user's "Favorites" list by movie ObjectId
-app.post('/users/:username/movies/:movieID', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.post('/users/:username/movies/:movieID', passport.authenticate('jwt', { session: false }),
+(req, res) => {
   Users.findOneAndUpdate({ 'Username': req.params.username }, {
     $push: { FavoriteMovies: req.params.movieID }
   },
